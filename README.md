7559816cadf4c052f8d18f6eeca70eb3edab3a0b
Malay Matwankar <matwankarmalay@gmail.com>
2018-03-06 22:50:20 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# airlines-iata-codes
-Provides with names of airlines based on IATA codes from Openflights.org
+# airlines-iata-code
+
+>This library provides with names of airlines based on IATA codes from Openflights.org
+
+## Install
+```javascript
+npm install airlines-iata-codes
+```
+
+## Usage
+var airlines = require('airlines-iata-codes');
+
+console.log(airlines.getAirline("Q5"));<br>
+// 40-Mile Air
---
+++ index.js
@@ -0,0 +1,19 @@
+let airlinesObject = require("./res/airlines.json");
+
+function getAirline(iataCode) {
+    let airline = [];
+    airlinesObject.filter(function (element) {
+        if (element["IATACode"] == iataCode) {
+            return airline.push(element["Airline"]);
+        } 
+    });
+
+    return airline.length>0?airline.toString():noSuchAirlineFoundError;
+}
+
+let noSuchAirlineFoundError = {
+    code: "NAF",
+    message: "We could not find the specified airline related to your IATA code, if you think this is a mistake please raise an issue at https://github.com/malaymatwankar/airlines-iata-codes/issues"
+}
+
+module.exports = { getAirline: getAirline };
